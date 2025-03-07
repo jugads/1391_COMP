c88994089c1438eedf8c371fd1795811114087a7
jugads <jugad.singh@westtown.edu>
2025-03-07 09:51:21 -0500

    removed joystick

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -19,8 +19,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotPeriodic() {
-    CommandScheduler.getInstance().run(); 
-    m_robotContainer.inputs();
+    CommandScheduler.getInstance().run();
   }
 
   @Override
